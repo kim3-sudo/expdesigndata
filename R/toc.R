09a69c631b25bf14a4d341bdf8b39ead05a0a7d0
Sejin Kim <kim3@kenyon.edu>
2022-01-26 15:18:04 -0500

    Initial commit

---
+++ R/toc.R
@@ -0,0 +1,25 @@
+#' toc
+#'
+#' \code{help(toc)} displays the table of contents for datasets.
+#'
+#' A guide to the datasets in expdesign. Datasets are listed under the Arguments section by the order they appear in the Nonparametric Statistical Methods textbook, 3ed. The reference name in the \code{nsm3data} package is given, along with the table number and the descriptor of the dataset.
+#'
+#' \tabular{ll}{
+#' \bold{Table} \tab \bold{Table Number - Description} \cr
+#' \code{shelflife} \tab {Problem 2.19S} \cr
+#' \code{hours} \tab {Problem 2.21} \cr
+#' \code{machines} \tab {Problem 2.23} \cr
+#' \code{flares} \tab {Problem 2.25} \cr
+#' \code{plasma} \tab {Problem 2.25} \cr
+#' \code{thickness} \tab {Problem 2.27} \cr
+#' \code{cellphones} \tab {Problem 2.28S} \cr
+#' }
+#' @examples
+#' \dontrun{
+#' help(toc)
+#' }
+#'
+
+toc <- function() {
+  stop("toc() is not directly invokable. Use help(toc) instead.")
+}
